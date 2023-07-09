fc88a8c01b249650cc3e006cd35398249e9b97ed
Navneet <navneet@mail.com>
2023-07-09 06:53:07 +0530

    improvement and middling

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ var settings = document.getElementById('toggle-open');
 var close = document.getElementById('toggle-close');
 var tooglebox = document.getElementById('toggle-box');
 
-settings.onclick = function(){
-    tooglebox.className = "open";
-    alert(tooglebox.className);
-}    
-close.onclick = function(){
-    tooglebox.className = "close";
-    alert(tooglebox.className);
-}
+  
 
 function signed(){
     window.location.href="signIN.html";
 }
+
+const toggleMenuBtn = document.getElementById('toggleMenuBtn');
+const menuContainer = document.getElementById('menuContainer');
+
+toggleMenuBtn.addEventListener('click', function() {
+  menuContainer.classList.toggle('show');
+});
